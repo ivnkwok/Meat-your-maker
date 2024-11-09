f13234a4a253c324292113176be5d891ce36cee4
Ivan Kwok <ivnkwok@gmail.com>
2024-11-08 22:17:39 -0800

    movement thumbs up

--- meat-your-maker/Player.gd
+++ meat-your-maker/Player.gd
@@ -1,14 +1,24 @@
 extends Sprite2D
 
+@onready var animation = $AnimationPlayer
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	animation.play("cow")
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	position += Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down").normalized() * delta * 16
+	var direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down").normalized()
+	animation.play()
+	if (direction.x == 0 && direction.y == 0):
+		animation.stop()
+	position += direction * delta * 50
+	if (direction.x < 0 && scale.x < 0):
+		scale.x *= -1
+	if (direction.x > 0 && scale.x > 0):
+		scale.x *= -1
 	#if (Input.is_action_pressed("ui_up")):
 		#position.y -= 1 * delta
 	#if (Input.is_action_pressed("ui_down")):
